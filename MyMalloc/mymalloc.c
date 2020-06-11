a083d61bfc25044a20b7b13f7f0a61f5864e5013
dheeraj <dnarasi1@binghamton.edu>
2020-06-11 02:18:02 -0400

    Basic implementation of malloc

--- MyMalloc/mymalloc.c
+++ MyMalloc/mymalloc.c
@@ -1,7 +1,27 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<unistd.h>
+#include<sys/types.h>
+
+void * mymalloc(size_t size){
+	void *ptr;
+	ptr = sbrk(0);
+	if (sbrk(size)==(void*)-1){
+		return NULL;
+	}
+	return ptr;
+}
+
+
+
 
 int main(){
-	printf("malloc\n");
+	char *p;
+	p = (char *)mymalloc(50);
+	if(p==NULL){
+		fprintf(stderr, "Error in mymalloc\n" );
+	}
+	strcpy(p, "Hi, I am Dheeraj, This is a test");
+	printf("And the output is: %s\n", p);
 }
