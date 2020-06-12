790fd1e0c85c9d98164bcdca78ecf9c25ec30839
dheeraj <dnarasi1@binghamton.edu>
2020-06-12 02:26:25 -0400

    implemented find free block and extend memory function

--- MyMalloc/mymalloc.c
+++ MyMalloc/mymalloc.c
@@ -6,7 +6,12 @@
 
 #define META_SIZE sizeof(struct meta_block)
 
+
+
 typedef struct meta_block *meta_block;
+
+
+void *head_base = NULL;
 
 struct meta_block
 {
@@ -16,16 +21,50 @@ struct meta_block
 
 };
 
+meta_block extend_space(meta_block last, size_t size);
+meta_block find_free(meta_block *last, size_t size);
+
 void * mymalloc(size_t size){
 	meta_block ptr;
-	ptr = sbrk(0);
-	if (sbrk(META_SIZE+size)==(void*)-1){
-		return NULL;
+	
+	if(!head_base) {
+		ptr = extend_space(NULL, size);
+		if(!ptr) return NULL;
+		head_base = ptr;
 	}
-	ptr->size = size;
+	else{
+		meta_block last = head_base;
+		ptr = find_free(&last, size);
+		if(!ptr){
+			ptr = extend_space(last, size);
+		}	
+	}
+	
 	return ptr;
 }
 
+meta_block find_free(meta_block *last, size_t size){
+	meta_block current = head_base;
+	while(current && !(current->isFree && current->size>=size)){
+		*last = current;
+		current = current->next;
+	}
+	return current;
+}
+
+meta_block extend_space(meta_block last, size_t size){
+	meta_block ptr;
+	ptr = sbrk(0);
+	if(sbrk(META_SIZE + size)==(void *)-1) return NULL;
+	ptr->size = size;
+	ptr->next = NULL;
+	if(last){
+		last->next = ptr;
+	}
+	
+	ptr->isFree = 0;
+	return ptr;
+}
 
 int main(){
 	char *p;
